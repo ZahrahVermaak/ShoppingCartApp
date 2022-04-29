bd9a7dba303306f8ad981b1272f53480bbe57c33
ZahrahVermaak <102726461+ZahrahVermaak@users.noreply.github.com>
2022-04-29 09:27:55 +0200

    added code

--- ShoppingCartAppSln/ShoppingAppCart/Program.cs
+++ ShoppingCartAppSln/ShoppingAppCart/Program.cs
@@ -7,9 +7,45 @@ namespace ShoppingAppCart
     internal class Program
     {
         static void Main(string[] args)
-        { 
-           
+        {
+            List<ShoppingCartItem> _items = new List<ShoppingCartItem>();
+
             ShoppingCart shoppingcart = new ShoppingCart();
+
+            var item = new ShoppingCartItem("Nike runner", 999);
+            //shoppingcart.Add(item);
+
+            item = new ShoppingCartItem("Sunfoil cooking oil", 169);
+            //shoppingcart.Add(item);
+
+            item = new ShoppingCartItem("SamsungLaptop", 3499);
+            //shoppingcart.Add(item);
+
+            item = new ShoppingCartItem("Evil Eye Necklace", 50);
+            //shoppingcart.Add(item);
+
+            item = new ShoppingCartItem("Chocolate Cake", 349);
+            //shoppingcart.Add(item);
+
+        }
+
+            /* Shoes shoes = new Shoes("Nike runner", 999);
+             _items.Add(shoes);
+
+             FishOil fishoil = new FishOil("Sunfoil cooking oil", 169);
+             _items.Add(fishoil);
+
+             LapTop laptop = new LapTop("SamsungLaptop", 3499);
+             _items.Add(laptop);
+
+             Necklace necklace = new Necklace("Evil eye necklace", 50);
+             _items.Add(necklace);
+
+             ChocolateCake chocolatecake = new ChocolateCake("Chocolate Cake", 349);
+             _items.Add(chocolatecake);*/
+
+
+            //ShoppingCart shoppingcart = new ShoppingCart();
 
             //ShoppingCartItem shoes = new Shoes(999);
             //ShoppingCartItem FishOil = new FishOil();
@@ -17,6 +53,6 @@ namespace ShoppingAppCart
             //Console.WriteLine("Sub Total: " + ShoppingCart.Getpriceofitems());
             //Console.WriteLine("VAT inlcuded: " + ShoppingCart.GetVATPriceOfItems());
 
-        }
+        
     }
 }
--- ShoppingCartAppSln/ShoppingLibrary/ShoppingCart.cs
+++ ShoppingCartAppSln/ShoppingLibrary/ShoppingCart.cs
@@ -6,7 +6,7 @@ namespace ShoppingLibrary
     public class ShoppingCart
     {
         private static int Getpriceofitems;
-        List<ShoppingCartItem> _items = new List<ShoppingCartItem>();
+      //  List<ShoppingCartItem> _items = new List<ShoppingCartItem>();
 
         //ShoppingCart cart = new ShoppingCart();
         // var item = new ShoppingCartItem("Shoes", 999);
@@ -15,32 +15,39 @@ namespace ShoppingLibrary
        
         private int _price;
 
-        public ShoppingCart()
-        {
-            Shoes shoes = new Shoes("Nike runner", 999);
-            _items.Add(shoes);
-
-            FishOil fishoil = new FishOil("Sunfoil cooking oil",  169);
-            _items.Add(fishoil);
-
-            LapTop laptop = new LapTop("SamsungLaptop", 3499);
-            _items.Add(laptop);
-
-            Necklace necklace = new Necklace("Evil eye necklace" ,50);
-            _items.Add(necklace);
-
-            ChocolateCake chocolatecake = new ChocolateCake("Chocolate Cake", 349);
-            _items.Add(chocolatecake);
-        }
-
         public int GetpriceOfItems()
         {
             return ShoppingCart.Getpriceofitems;
         }
         public int GetVATPriceOfItems()
         {
-            return _price * 15 / 100;
+            return _price * 115;
         }
+
+
+
+
+
+
+        /* public ShoppingCart()
+         {
+             Shoes shoes = new Shoes("Nike runner", 999);
+             _items.Add(shoes);
+
+             FishOil fishoil = new FishOil("Sunfoil cooking oil",  169);
+             _items.Add(fishoil);
+
+             LapTop laptop = new LapTop("SamsungLaptop", 3499);
+             _items.Add(laptop);
+
+             Necklace necklace = new Necklace("Evil eye necklace" ,50);
+             _items.Add(necklace);
+
+             ChocolateCake chocolatecake = new ChocolateCake("Chocolate Cake", 349);
+             _items.Add(chocolatecake);
+         }*/
+
+
         // other option is to use the get and set method
     }
 
